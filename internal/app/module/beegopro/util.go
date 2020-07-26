6098ad337e8593f08dec4ea1f9a6b096b83a85b3
myxy99 <40028336+myxy99@users.noreply.github.com>
2020-07-26 13:20:49 +0800

    Update util.go

--- internal/app/module/beegopro/util.go
+++ internal/app/module/beegopro/util.go
@@ -39,7 +39,7 @@ func (c *RenderFile) write(filename string, buf string) (err error) {
 	name := path.Base(filename)
 
 	if utils.IsExist(filename) {
-		bakName := fmt.Sprintf("%s/%s.%s.bak", filePathBak, filepath.Base(name), time.Now().Format("2006.01.02.15.04.05"))
+		bakName := fmt.Sprintf("%s.%s.bak", filepath.Base(name), time.Now().Format("2006.01.02.15.04.05"))
 		beeLogger.Log.Infof("bak file '%s'", bakName)
 		if err := os.Rename(filename, bakName); err != nil {
 			err = errors.New("file is bak error, path is " + bakName)
